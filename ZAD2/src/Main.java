3bd36eed2e74b9524bcb29170dcae104f6cff14c
zelek684 <73654594+zelek684@users.noreply.github.com>
2021-10-11 22:51:52 +0200

    CREATE

--- ZAD2/src/Main.java
+++ ZAD2/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(1+2+3+4+5+6+7+8+9+10);
+    public static void main(String[] args) throws Exception {
+        int iloraz=1;
+        for(int i=1;i<=10;i++){
+            iloraz *= i;
+        }
+        System.out.println(iloraz);
     }
-}
+    }
+
