d0a0f08ed5671fdb7ba17448473c7ed1323d813a
zelek684 <angelikalazoryszyn684@wp.pl>
2022-01-28 18:34:30 +0100

    CREATE

---
+++ WordsInHashSetDemo/src/WordsInTreeSetDemo.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class WordsInTreeSetDemo
+{
+    public static void main(String[] args) throws FileNotFoundException {
+        Set<String> words = new TreeSet<String>();
+        long totalTime = 0;
+
+        File file = new File("src/alice30.txt");
+        Scanner in = new Scanner(file);
+        while (in.hasNext()) {
+            String word = in.next();
+            long callTime = System.currentTimeMillis();
+            words.add(word);
+            callTime = System.currentTimeMillis() - callTime;
+            totalTime += callTime;
+        }
+
+        Iterator<String> iter = words.iterator();
+        for (int j = 1; j <= 20; ++j) {
+            System.out.println(iter.next());
+        }
+
+        System.out.println(". . .");
+        System.out.println(words.size() + " unikalnych. " + totalTime + " milisekund.");
+
+    }
+}
